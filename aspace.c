045564f55cc794eb208bf507372c412bc8162914
Thomas Delrue <thomasdelrue@gmail.com>
2016-01-14 13:21:15 +0100

    first commit

---
+++ aspace.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int global;
+
+int main()
+{
+    int local = 5;
+    void *p = malloc(128);
+
+    printf("Address of main is %p\n", main);
+    printf("Address of global is %p\n", &global);
+    printf("Address of local is %p\n", &local);
+    printf("Address of p is %p\n", p);
+
+    return 0;
+}
